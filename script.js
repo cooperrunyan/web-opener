245eb31591c71b353dd96da745cf9b9ddc9af0d0
cooperrunyan <cooperrunyanspam@gmail.com>
2022-03-25 23:17:45 -0700

    Changed to search query

--- script.js
+++ script.js
@@ -1,31 +1,5 @@
 #!/usr/bin/env node
 
 const opn = require('opn');
-const fs = require('fs');
-
-if (process.argv[2] === 'var') {
-  const vars = JSON.parse(fs.readFileSync(__dirname + '/vars.json'));
-  vars[process.argv[3]] = process.argv[4];
-  fs.writeFileSync(__dirname + '/vars.json', JSON.stringify(vars, null, 2));
-} else if (process.argv[2] === 'get') {
-  const vars = JSON.parse(fs.readFileSync(__dirname + '/vars.json'));
-  Object.entries(vars).forEach(([key, value]) => {
-    console.log(`"${key}" = "${value}"`);
-  });
-} else if (process.argv[2] === 'delete') {
-  const vars = JSON.parse(fs.readFileSync(__dirname + '/vars.json'));
-  delete vars[process.argv[3]];
-  fs.writeFileSync(__dirname + '/vars.json', JSON.stringify(vars, null, 2));
-} else {
-  let link = process.argv[2];
-  const vars = JSON.parse(fs.readFileSync(__dirname + '/vars.json'));
-  if (vars[link]) {
-    opn(vars[link]);
-  } else {
-    link = link.replace('https://', '').replace('http://', '');
-    if (link.split('.').length === 1) link = `https://www.${link}.com`;
-    else if (link.split('.').length === 2) link = `https://www.${link}`;
-    else if (link.split('.').length === 3) link = `https://${link}`;
-    opn(link);
-  }
-}
+const searchQuery = process.argv.slice(2);
+opn(searchQuery[0] ? `https://google.com/search?q=${searchQuery.join('+')}` : `https://google.com`);
